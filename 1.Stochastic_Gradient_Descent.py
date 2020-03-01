36c10d013ca1a6f2611ac1989901efae03117d36
williamjiamin <lxodgf@163.com>
2020-03-02 00:03:47 +0800

    add algo

---
+++ 1.Stochastic_Gradient_Descent.py
@@ -0,0 +1,17 @@
+def make_prediction(input_row, coefficients):
+    out_put_y_hat = coefficients[0]
+    for i in range(len(row) - 1):
+        out_put_y_hat += coefficients[i + 1] * input_row[i]
+    return out_put_y_hat
+
+
+test_dataset = [[1, 1],
+                [2, 3],
+                [4, 3],
+                [3, 2],
+                [5, 5]]
+test_coefficients = [0.4, 0.8]
+
+for row in test_dataset:
+    y_hat = make_prediction(row, test_coefficients)
+    print("Expected = %.3f, Our_Prediction = %.3f" % (row[-1], y_hat))
