e20eba564e077009f845352897a3d1dbddb9b137
williamjiamin <lipai_wang@163.com>
2020-12-29 12:47:45 +0800

    add abalone example

--- X.Kaggle数据用PurePython实战/case05_abalone_age_using_KNN.py
+++ X.Kaggle数据用PurePython实战/case05_abalone_age_using_KNN.py
@@ -19,9 +19,6 @@ def read_our_csv_file(filename):
     return dataset
 
 
-# dataset = read_our_csv_file('abalone.csv')
-# print(dataset)
-
 # 2.数据类型转换（str to float）
 
 
@@ -65,7 +62,7 @@ def normalize_our_data(dataset, min_and_max_list):
 def k_fold_cross_validation(dataset, n_folds):
     dataset_split = list()
     dataset_copy = list(dataset)
-    every_fold_size = (len(dataset) / n_folds)
+    every_fold_size = int(len(dataset) / n_folds)
     for i in range(n_folds):
         fold = list()
         while len(fold) < every_fold_size:
@@ -81,4 +78,82 @@ def calculate_our_model_accuracy(actual, predicted):
     for i in range(len(actual)):
         if actual[i] == predicted[i]:
             correct_counter += 1
-    return correct_counter / float(len(actual)) ** 100.0
+    return correct_counter / float(len(actual)) * 100.0
+
+
+# 7.给我们的算法进行评估（打分）
+
+def how_good_is_our_algo(dataset, algo, n_folds, *args):
+    folds = k_fold_cross_validation(dataset, n_folds)
+    scores = list()
+    for fold in folds:
+        train_dataset = list(folds)
+        train_dataset.remove(fold)
+        train_dataset = sum(train_dataset, [])
+        test_dataset = list()
+        for row in fold:
+            row_copy = list(row)
+            test_dataset.append(row_copy)
+            row_copy[-1] = None
+        predicted = algo(train_dataset, test_dataset, *args)
+        actual = [row[-1] for row in fold]
+        accuracy = calculate_our_model_accuracy(actual, predicted)
+        scores.append(accuracy)
+    return scores
+
+
+# 8.计算欧几里德举例
+
+def calculate_euclidiean_distance(row1, row2):
+    distance = 0.0
+    for i in range(len(row1) - 1):
+        distance += (row1[i] - row2[i]) ** 2
+    return sqrt(distance)
+
+
+# 9.找到最近的k个点
+def get_our_neighbors(train_dataset, test_row, num_of_neighbors):
+    distances = list()
+    for train_dataset_row in train_dataset:
+        dist = calculate_euclidiean_distance(test_row, train_dataset_row)
+        distances.append((train_dataset_row, dist))
+    distances.sort(key=lambda every_tuple: every_tuple[1])
+    neighbors = list()
+    for i in range(num_of_neighbors):
+        neighbors.append(distances[i][0])
+    return neighbors
+
+
+# 10.做预测
+def make_prediction(train_dataset, test_row, num_of_neighbors):
+    neighbors = get_our_neighbors(train_dataset, test_row, num_of_neighbors)
+    output = [row[-1] for row in neighbors]
+    our_prediction = max(set(output), key=output.count)
+    return our_prediction
+
+
+# 11.运用KNN算法
+def get_our_prediction_using_knn_algo(train_dataset, test_dataset, num_of_neighbors):
+    predictions = list()
+    for test_row in test_dataset:
+        our_prediction = make_prediction(train_dataset, test_row, num_of_neighbors)
+        predictions.append(our_prediction)
+    return predictions
+
+
+seed(1)
+dataset = read_our_csv_file('abalone.csv')
+for i in range(1, len(dataset[0])):
+    change_string_to_float(dataset, i)
+
+change_string_to_int(dataset, 0)
+
+# print(dataset)
+
+n_folds = 10
+
+num_neighbors = 7
+scores = how_good_is_our_algo(dataset, get_our_prediction_using_knn_algo, n_folds, num_neighbors)
+
+print('Our model\'s scores are : %s' % scores)
+print('The mean accuracy is :%.3f%%' % (sum(scores) / float(len(scores))))
